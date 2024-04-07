378d111d4a273dd21d5a94e9ea3ff4378e7f63fe
fidesy <0xfidesy@gmail.com>
2024-04-08 01:34:34 +0300

    try fix outbox

--- internal/pkg/outbox-processor/outbox_processor.go
+++ internal/pkg/outbox-processor/outbox_processor.go
@@ -3,8 +3,9 @@ package outbox_processor
 import (
 	"context"
 	"github.com/fidesy-pay/invoices-service/internal/pkg/models"
+	"github.com/fidesy/sdk/common/logger"
 	"github.com/samber/lo"
-	"log"
+	"go.uber.org/zap"
 	"time"
 )
 
@@ -47,7 +48,11 @@ func (s *Service) Publish(ctx context.Context) {
 func (s *Service) publish(ctx context.Context) {
 	invoicesOutbox, err := s.storage.ListInvoicesOutbox(ctx, 100)
 	if err != nil {
-		log.Printf("storage.ListInvoicesOutbox: %v", err)
+		logger.Errorf("storage.ListInvoicesOutbox: %v", err)
+		return
+	}
+
+	if len(invoicesOutbox) == 0 {
 		return
 	}
 
@@ -55,10 +60,12 @@ func (s *Service) publish(ctx context.Context) {
 		s.kafkaProducer.ProduceMessage(invoicesTopic, []byte(invoice.Message))
 	}
 
-	err = s.storage.DeleteInvoicesOutbox(ctx, lo.Map(invoicesOutbox, func(user *models.InvoiceOutbox, _ int) int {
-		return user.ID
+	err = s.storage.DeleteInvoicesOutbox(ctx, lo.Map(invoicesOutbox, func(invoice *models.InvoiceOutbox, _ int) int {
+		return invoice.ID
 	}))
 	if err != nil {
-		log.Printf("storage.DeleteInvoicesOutbox: %v", err)
+		logger.Errorf("storage.DeleteInvoicesOutbox: %v", err,
+			zap.Int("invoices_length:", len(invoicesOutbox)),
+		)
 	}
 }
